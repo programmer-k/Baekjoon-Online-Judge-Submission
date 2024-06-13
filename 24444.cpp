a2b02f042825329327baf879bdaf33f733ce3e79
programmer-k <programmer-j@naver.com>
2024-06-13 09:46:42 +0900

    Solve 24444.

---
+++ 24444.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, m, r;
+vector<int> edges[100001];
+int visited[100001];
+
+void GetInput() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m >> r;
+  for (int i = 0; i < m; ++i) {
+    int from, to;
+    cin >> from >> to;
+    edges[from].push_back(to);
+    edges[to].push_back(from);
+  }
+}
+
+void BreadthFirstSearch() {
+  int visit_cnt = 1;
+  queue<int> q;
+  visited[r] = visit_cnt;
+  q.push(r);
+
+  while (!q.empty()) {
+    int node = q.front();
+    q.pop();
+
+    for (unsigned int i = 0; i < edges[node].size(); ++i) {
+      int next_node = edges[node][i];
+
+      if (!visited[next_node]) {
+        visited[next_node] = ++visit_cnt;
+        q.push(next_node);
+      }
+    }
+  }
+}
+
+void Solve() {
+  for (int i = 1; i <= n; ++i)
+    sort(edges[i].begin(), edges[i].end());
+
+  BreadthFirstSearch();
+
+  for (int i = 1; i <= n; ++i)
+    cout << visited[i] << '\n';
+}
+
+int main(void) {
+  GetInput();
+  Solve();
+  return 0;
+}

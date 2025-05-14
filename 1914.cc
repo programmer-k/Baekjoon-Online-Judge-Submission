c531261250afd8c86e65af3b6807df6955d1af4c
programmer-k <programmer-j@naver.com>
2025-05-14 17:44:53 +0900

    Solve 1914.

--- 1914.cc
+++ 1914.cc
@@ -5,6 +5,7 @@
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
 int n;
@@ -15,6 +16,17 @@ void GetInput() {
   ios::sync_with_stdio(false);
 
   cin >> n;
+}
+
+__int128_t Power(int base, int exponent) {
+  if (exponent == 0)
+    return 1;
+  
+  __int128_t val = Power(base, exponent / 2);
+  if (exponent % 2 == 0)
+    return val * val;
+  else
+    return val * val * base;
 }
 
 void Hanoi(int val, int from, int stopover, int to) {
@@ -28,12 +40,23 @@ void Hanoi(int val, int from, int stopover, int to) {
   Hanoi(val - 1, stopover, from, to);
 }
 
+string ToString(__int128_t val) {  
+  string str;
+  while (val > 0) {
+    str.push_back(val % 10 + '0');
+    val /= 10;
+  }
+  reverse(str.begin(), str.end());
+  return str;
+}
+
 void Solve() {
-  stringstream ss;
+  /*stringstream ss;
   ss << fixed << setprecision(0) << pow(2, n);
   string answer = ss.str();
   answer.back() -= 1;
-  cout << answer << '\n';
+  cout << answer << '\n';*/
+  cout << ToString(Power(2, n) - 1) << '\n';
   
   if (n > 20)
     return;

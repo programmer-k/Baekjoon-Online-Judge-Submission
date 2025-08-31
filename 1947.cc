c1e64050c67216426e3f7e867f27bca3507b5cb3
programmer-k <programmer-j@naver.com>
2025-08-31 22:08:34 +0900

    Solve 1947.

---
+++ 1947.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+using namespace std;
+
+const int kDivisor = 1'000'000'000;
+int n;
+vector<int64_t> dp(1'000'001);
+
+void GetInput() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin >> n;
+}
+
+void Solve() {
+  dp[2] = 1;
+
+  for (int i = 3; i <= n; ++i)
+    dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]) % kDivisor;
+
+  cout << dp[n] << '\n';
+}
+
+int main() {
+  GetInput();
+  Solve();
+  return 0;
+}

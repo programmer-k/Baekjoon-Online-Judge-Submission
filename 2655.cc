58d8a8665027d2a60d1156be14087c3d1f7a2143
programmer-k <programmer-j@naver.com>
2025-09-19 00:19:39 +0900

    Update

--- 2655.cc
+++ 2655.cc
@@ -7,6 +7,7 @@ using namespace std;
 
 struct Brick {
   int area;
+  int height;
   int weight;
   int index;
 
@@ -30,30 +31,37 @@ void GetInput() {
   for (int i = 0; i < n; ++i) {
     int area, height, weight;
     cin >> area >> height >> weight;
-    bricks.push_back({area, weight, i + 1});
+    bricks.push_back({area, height, weight, i + 1});
   }
 }
 
 void Solve() {
   sort(bricks.begin(), bricks.end(), greater<Brick>());
 
-  vector<pair<int, int>> dp(n, make_pair(1, -1));
+  vector<pair<int, int>> dp(n);
   for (int i = 0; i < n; ++i) {
+    dp[i].first = bricks[i].height;
+    dp[i].second = -1;
     for (int j = 0; j < i; ++j) {
-      if (bricks[i].weight < bricks[j].weight && dp[i].first <= dp[j].first) {
-        dp[i].first = dp[j].first + 1;
+      if (bricks[i].weight < bricks[j].weight && dp[i].first < dp[j].first + bricks[i].height) {
+        dp[i].first = dp[j].first + bricks[i].height;
         dp[i].second = j;
       }
     }
   }
 
+  int count = 0;
+  vector<int> answers;
   int index = max_element(dp.begin(), dp.end()) - dp.begin();
-  cout << dp[index].first << '\n';
-
   while (index != -1) {
-    cout << bricks[index].index << '\n';
+    answers.push_back(bricks[index].index);
+    ++count;
     index = dp[index].second;
   }
+
+  cout << count << '\n';
+  for (int answer : answers)
+    cout << answer << '\n';
 }
 
 int main() {

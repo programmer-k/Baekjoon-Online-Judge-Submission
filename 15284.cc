4691ac987308c7ec1887a8b8d2494aca01b782a3
programmer-k <programmer-j@naver.com>
2025-11-02 13:09:12 +0900

    Solve 15284.

---
+++ 15284.cc
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int kMod = 1'000'000'007;
+
+int n;
+vector<int> arr;
+
+void GetInput() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+
+  arr.resize(n);
+  for (int i = 0; i < n; ++i)
+    cin >> arr[i];
+}
+
+int Power(int base, int exponent) {
+  if (exponent == 0)
+    return 1;
+
+  int64_t half = Power(base, exponent / 2);
+  if (exponent % 2 == 0)
+    return half * half % kMod;
+  else
+    return half * half * base % kMod;
+}
+
+void Solve() {
+  sort(arr.begin(), arr.end());
+
+  int64_t answer = 0;
+  for (int i = 0; i < n; ++i) {
+    int64_t count = Power(2, n - 1 - i);
+    int diff = arr[n - 1 - i] - arr[i];
+    answer += count * diff;
+    answer %= kMod;
+  }
+
+  cout << answer << '\n';
+}
+
+int main() {
+  GetInput();
+  Solve();
+  return 0;
+}

dd9a41626fc8b44d0e2496d22f9183f00c3ff67f
programmer-k <programmer-j@naver.com>
2026-01-03 20:39:35 +0900

    Update

--- 1509.cc
+++ 1509.cc
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <limits>
 #include <string>
+#include <vector>
 using namespace std;
 
 string str;
 int is_palindrome[2'500][2'500];
-int dp[2'500][2'500];
 
 void GetInput() {
   cin.tie(nullptr);
@@ -27,33 +27,20 @@ int IsPalindrome(int from, int to) {
     return is_palindrome[from][to] = 0;
 }
 
-int CalculateMinPalindromeCount(int from, int to) {
-  if (dp[from][to] != -1)
-    return dp[from][to];
-
-  if (is_palindrome[from][to] == 1)
-    return dp[from][to] = 1;
-
-  int min_val = numeric_limits<int>::max();
-  for (int mid = from; mid < to; ++mid) {
-    int val1 = CalculateMinPalindromeCount(from, mid);
-    int val2 = CalculateMinPalindromeCount(mid + 1, to);
-    min_val = min(min_val, val1 + val2);
-  }
-
-  return dp[from][to] = min_val;
-}
-
 void Solve() {
   memset(is_palindrome, -1, sizeof(is_palindrome));
-  memset(dp, -1, sizeof(dp));
-
   for (int i = 0; i < ssize(str); ++i)
     for (int j = i; j < ssize(str); ++j)
       if (is_palindrome[i][j] == -1)
         IsPalindrome(i, j);
 
-  cout << CalculateMinPalindromeCount(0, ssize(str) - 1) << '\n';
+  vector<int> dp(ssize(str), numeric_limits<int>::max());
+  for (int i = 0; i < ssize(dp); ++i)
+    for (int j = 0; j <= i; ++j)
+      if (is_palindrome[j][i] == 1)
+        dp[i] = min(dp[i], (j - 1 >= 0 ? dp[j - 1] : 0) + 1);
+
+  cout << dp[ssize(str) - 1] << '\n';
 }
 
 int main() {

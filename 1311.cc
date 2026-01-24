8479539f18bcba8fbb7df3e57324516b184b5246
programmer-k <programmer-j@naver.com>
2026-01-24 16:03:10 +0900

    Update

--- 1311.cc
+++ 1311.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int n;
 int costs[20][20];
-int dp[20][1 << 20];
+int dp[1 << 20];
 
 void GetInput() {
   cin.tie(nullptr);
@@ -22,21 +22,20 @@ int CalculateMinCost(int person, int visited) {
   if (person == n)
     return 0;
 
-  if (dp[person][visited] != -1)
-    return dp[person][visited];
+  if (dp[visited] != -1)
+    return dp[visited];
 
-  dp[person][visited] = numeric_limits<int>::max();
+  dp[visited] = numeric_limits<int>::max();
   for (int job = 0; job < n; ++job) {
     if (visited & (1 << job))
       continue;
 
     int next_visited = visited | (1 << job);
-    dp[person][visited] =
-        min(dp[person][visited],
-            CalculateMinCost(person + 1, next_visited) + costs[person][job]);
+    dp[visited] = min(dp[visited], CalculateMinCost(person + 1, next_visited) +
+                                       costs[person][job]);
   }
 
-  return dp[person][visited];
+  return dp[visited];
 }
 
 void Solve() {
